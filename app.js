f0f0b4fa2ddbb9a614db089d1e521ff6b4ffac0a
Nelson Cothran <nelsonc19@gmail.com>
2017-06-11 13:14:28 -0400

    js updates

--- app.js
+++ app.js
@@ -6,41 +6,37 @@ const summary = document.querySelector('.summary');
 const network = document.querySelector('.network');
 const rating = document.querySelector('.rating');
 
-button.addEventListener('click', apiCall);
-function apiCall() {
-  fetch('http://api.tvmaze.com/shows').then(function(response) {
+let showArray = [];
+
+button.addEventListener('click', getShow);
+
+(function apiCall() {
+  let pageNum = Math.floor(Math.random() * (5 - 0)) + 0;
+  fetch('http://api.tvmaze.com/shows?page=' + pageNum).then(function(response) {
   	// Convert to JSON
   	return response.json();
   }).then(function(results) {
-    let randomNumber = Math.round(Math.random() * (240 - 0) + 1);
-    let sLength = (results[randomNumber].summary).length;
-    if (sLength > 475-3) {
-      resultPoster.src = results[randomNumber].image.medium;
-      summary.innerHTML = (results[randomNumber].summary).substring(0, 495 - 3) + '...';
-      network.innerText = results[randomNumber].network.name;
-      rating.innerText = results[randomNumber].rating.average + '/10';
-      resultTitle.innerText = results[randomNumber].name;
-      if (resultsContainer.classList == 'results-container slide-class') {
-        resultsContainer.classList.remove('slide-class');
-        resultsContainer.classList.add('slide-class');
-      } else if(resultsContainer.classList == 'results-container') {
-        resultsContainer.classList.add('slide-class');
-      }
-    } else {
-      resultPoster.src = results[randomNumber].image.medium;
-      summary.innerHTML = results[randomNumber].summary;
-      network.innerText = results[randomNumber].network.name;
-      rating.innerText = results[randomNumber].rating.average + '/10';
-      resultTitle.innerHTML = results[randomNumber].name;
-      if (resultsContainer.classList == 'results-container') {
-        resultsContainer.classList.add('slide-class');
-        resultsContainer.classList.remove('slide-class');
-      } else if(resultsContainer.classList == 'results-container') {
-        resultsContainer.classList.add('slide-class');
-      }
-    }
+    showArray = results;
   }).catch(function(err) {
   	// Error :(
     console.log('error');
   });
+})();
+
+function getShow() {
+  let randomNumber = Math.floor(Math.random() * (240 - 0) + 0);
+  let prevNumber = randomNumber;
+  prevNumber == randomNumber ? randomNumber = Math.floor(Math.random() * (240 - 0) + 0) : randomNumber = Math.floor(Math.random() * (240 - 0) + 0);
+  let sLength = (showArray[randomNumber].summary).length;
+  if (sLength > 475-3) {
+    summary.innerHTML = (showArray[randomNumber].summary).substring(0, 495 - 3) + '...';
+  } else {
+    summary.innerHTML = showArray[randomNumber].summary;
+  }
+  resultPoster.src = showArray[randomNumber].image.medium;
+  network.innerText = showArray[randomNumber].network.name;
+  showArray[randomNumber].name == null ? resultTitle.innerText = '' : resultTitle.innerText = showArray[randomNumber].name;
+  showArray[randomNumber].rating.average == null ? rating.innerText = 'unrated' : rating.innerText = showArray[randomNumber].rating.average + '/10';
+  resultsContainer.style.visibility = 'visible';
+  resultsContainer.style.animation = 'slideresults .3s cubic-bezier(.17,.67,.31,.89)';
 }
